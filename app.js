5ac12370e86e16bab77086a0d289e533c016d368
yanica svensson <sweets86_@hotmail.com>
2021-02-04 22:26:13 +0100

    starts

--- app.js
+++ app.js
@@ -11,11 +11,10 @@ const path = require("path");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 console.log(process.env.STRIPE_SECRET_KEY);
 
-app.use("/", express.json());
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/build"),express.json());
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
